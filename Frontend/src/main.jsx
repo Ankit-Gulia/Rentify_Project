4e39926215bafdb167dce043a1f73dfa4a3766e2
Ankit Gulia <Ankitgulia67@gmail.com>
2026-04-05 13:45:48 +0530

    update something

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import React from 'react';
 import App from './App';
-import { AuthProvider } from "./AuthContext";
+import { AuthProvider } from "./components/AuthContext";
 
 createRoot(document.getElementById('root')).render(
     <AuthProvider>
